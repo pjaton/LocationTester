a110e95f843465479925b251d1af169003ec357c
Patrice Jaton <patrice@4gates.org>
2012-02-19 20:25:18 -0700

    Initial Commit

---
+++ LocationTester/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  LocationTester
+//
+//  Created by Patrice Jaton on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ LocationTester/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  LocationTester
+//
+//  Created by Patrice Jaton on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
